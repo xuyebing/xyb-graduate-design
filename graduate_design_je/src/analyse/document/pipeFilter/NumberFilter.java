efbaf280cb097606c210c4ad316a649169f91eab
Yebing Xu <yebingxu@gmail.com>
2012-03-07 11:58:04 +0800

    first commit to my graduate design

---
+++ graduate_design_je/src/analyse/document/pipeFilter/NumberFilter.java
@@ -0,0 +1,20 @@
+package analyse.document.pipeFilter;
+
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * @author Xu Yebing
+ * NumberFilter类 用于过滤分词结果中的数字和"."(包括"1.1.1"这样的形式)
+ */
+public class NumberFilter {
+
+	/**
+	 * isNumWithPunctuate 判断参数word是否仅由"数字" , "." , "-", "_"
+	 * @return
+	 */
+	public static boolean isNumWithPunctuate(String word) {
+		String regex = "[0-9.\\-_]+";
+		return Pattern.matches(regex, word);
+	}
+}
